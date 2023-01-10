ff6c6b2f26540b5c740d8b23d7ca371aee3aabb3
Julio Jimenez <julio@julioj.com>
2023-01-09 23:56:44 -0500

    defclass
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- clickhouse-cl.lisp
+++ clickhouse-cl.lisp
@@ -3,10 +3,24 @@
   (:use :cl :sxql :dexador)
   (:shadowing-import-from :dexador "GET")
   (:shadowing-import-from :dexador "DELETE")
-  (:export :connect))
+  (:export :connect
+	   :database))
 
 (in-package :clickhouse)
 
 (defun connect ()
   "Connect to ClickHouse Instance"
   (print "Connected! (not really)"))
+
+(defclass database ()
+  ((host
+    :initarg :host)
+   (port
+    :initarg :port
+    :initform 8443)
+   (username
+    :initarg :username
+    :initform "default")
+   (password
+    :initarg :password)))
+
