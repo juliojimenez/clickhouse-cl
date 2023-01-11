2c0fcc51f0a6546163e45da388e38b9ad4d938cb
Julio Jimenez <julio@julioj.com>
2023-01-11 13:01:42 -0500

    more class work
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- clickhouse-cl.lisp
+++ clickhouse-cl.lisp
@@ -14,13 +14,22 @@
 
 (defclass database ()
   ((host
-    :initarg :host)
+    :initarg :host
+    :initform (error "Must provide a hostname.")
+    :accessor host
+    :documentation "ClickHouse database hostname.")
    (port
     :initarg :port
-    :initform 8443)
+    :initform 8443
+    :accessor port
+    :documentation "ClickHouse database port, i.e. 8443 or 8123.")
    (username
     :initarg :username
-    :initform "default")
+    :initform "default"
+    :accessor username
+    :documentation "ClickHouse database username, default username is default.")
    (password
-    :initarg :password)))
+    :initarg :password
+    :accessor password
+    :documentation "Clickhouse database password.)))
 
