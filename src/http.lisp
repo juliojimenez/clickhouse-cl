f670aece29b77bbc5dc23dcd9a712e8ce91d850a
Julio Jimenez <julio@julioj.com>
2023-02-11 13:37:00 -0500

    formatter setup
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- src/http.lisp
+++ src/http.lisp
@@ -3,19 +3,20 @@
   (:shadowing-import-from "DEXADOR" "GET")
   (:shadowing-import-from "DEXADOR" "POST")
   (:shadowing-import-from "DEXADOR" "DELETE")
-  ;(:shadowing-import-from "DEXADOR.BACKEND.USOCKET" "WRITE-MULTIPART-CONTENT")
   (:export :http-get
            :http-post))
   
 (in-package :clickhouse.http)
 
 (defun http-get (host-slot port-slot ssl-slot uri)
+  "HTTP handler for GET endpoints."
   (multiple-value-bind (body status response-headers uri stream)
       (get (clickhouse.utils:format-url host-slot port-slot ssl-slot uri)
 		   :force-string t)
     (values body)))
 
 (defun http-post (host-slot port-slot ssl-slot content timeout)
+  "HTTP handler for POST endpoints."
   (multiple-value-bind (body status response-header uri stream)
       (post (clickhouse.utils:format-url host-slot port-slot ssl-slot "")
 		    :content content
